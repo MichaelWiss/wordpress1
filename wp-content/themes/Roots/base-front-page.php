d7eaf7aca13832f350798f45a0539851c5f1fa7a
MichaelWiss <michael.wiss@gmail.com>
2013-12-11 15:56:33 -0500

    removed other themes

--- wp-content/themes/Roots/base-front-page.php
+++ wp-content/themes/Roots/base-front-page.php
@@ -491,6 +491,37 @@
 
   <div id="almostfooter"  data-stellar-background-ratio="0.2" class:"container-fluid">
   <?php get_template_part('templates/footer'); ?>
+
+   <div class="row">
+          <div class="col-md-2 col-md-offset-2 text-center">
+            <div class="service-item">
+              <a class="github" href="https://github.com/MichaelWiss"><i class="service-icon fa fa-github"></i></a>
+              <h4>Github</h4>
+              <p>Did your navigation system shut down in the middle of that asteroid field? We can repair any dings and scrapes to your spacecraft!</p>
+            </div>
+          </div>
+          <div class="col-md-2 text-center">
+            <div class="service-item">
+              <a class="instagram" href="http://wegrams.com/knish72"><i class="service-icon fa fa-instagram"></i></a>
+              <h4>Instagram</h4>
+              <p>Need to know how magnets work? Our problem solving solutions team can help you identify problems and conduct exploratory research.</p>
+            </div>
+          </div>
+          <div class="col-md-2 text-center">
+            <div class="service-item">
+              <a class="linkedin" href="http://www.linkedin.com/pub/michael-wiss/3b/55a/7a0"><i class="service-icon fa fa-linkedin-square"></i></a>
+              <h4>Linkedin</h4>
+              <p>Planning a time travel trip to the middle ages? Preserve the space time continuum by blending in with period accurate armor and weapons.</p>
+            </div>
+          </div>
+          <div class="col-md-2 text-center">
+            <div class="service-item">
+              <a class="painting" href="http://michaelrwiss.com/"><i class="service-icon fa fa-pencil"></i></a>
+              <h4>Pencil Sharpening</h4>
+              <p>We've been voted the best pencil sharpening service for 10 consecutive years. If you have a pencil that feels dull, we'll get it sharp!</p>
+            </div>
+          </div>
+          </div>
   </div>
 
 
