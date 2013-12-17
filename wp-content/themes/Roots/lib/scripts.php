0fc9477a713b6125d5bb229d10e52062c9dc25a7
MichaelWiss <michael.wiss@gmail.com>
2013-12-17 18:22:29 -0500

    updated revslider

--- wp-content/themes/Roots/lib/scripts.php
+++ wp-content/themes/Roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5ee0d7ef32bff6193380cdeca6f0f082');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '503a455504eae90d7baf4a3b9fa2815d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
