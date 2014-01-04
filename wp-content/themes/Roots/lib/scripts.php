411b63925b3e9cfe7a12db7703311d7b0706c85e
MichaelWiss <michael.wiss@gmail.com>
2014-01-04 14:34:22 -0500

    fixed flexslider nav

--- wp-content/themes/Roots/lib/scripts.php
+++ wp-content/themes/Roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a449e956a89bf6d1ffe336c2fe3f7377');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ce21032a649c683062f7899b8d1bf92a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
